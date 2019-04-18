89f355c94982c186835cab7a50997ea0c2d01396
CL <cao_com@qq.com>
2019-04-18 18:59:15 +0800

    20190418

---
+++ app/src/main/java/com/nfc/electronicseal/MainActivity.java
@@ -0,0 +1,13 @@
+package com.nfc.electronicseal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
